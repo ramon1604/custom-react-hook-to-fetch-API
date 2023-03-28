35631b17eb77fcb2f240f08cce22de02c10b3f83
Ramon M Cott <ramoncott@hotmail.com>
2023-03-28 14:24:54 +0000

    Implementing useCallback to remove ESLint messages

--- src/useIterator.js
+++ src/useIterator.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 let arrLength = true;
 
@@ -7,36 +7,39 @@ export default function useIterator(url) {
   const [current, setCurrent] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const addUser = async () => {
-    setLoading(true);
-    const add = await fetch(url);
-    const res = await add.json();
-    const {
-      name: { first, last },
-      picture: { large }
-    } = res.results[0];
-    const count = userList.length || 0;
-    setUserList([
-      ...userList,
-      { id: count, fullName: `${first} ${last}`, image: large }
-    ]);
-    setCurrent({ id: count, fullName: `${first} ${last}`, image: large });
-    setLoading(false);
-  };
+  const addUserCallback = useCallback(() => {
+    (
+      async () => {
+        setLoading(true);
+        const add = await fetch(url);
+        const res = await add.json();
+        const {
+          name: { first, last },
+          picture: { large }
+        } = res.results[0];
+        const count = userList.length || 0;
+        setUserList([
+          ...userList,
+          { id: count, fullName: `${first} ${last}`, image: large }
+        ]);
+        setCurrent({ id: count, fullName: `${first} ${last}`, image: large });
+        setLoading(false);
+      }
+    )()
+  }, [url, userList])
 
   useEffect(() => {
     if (arrLength) {
-      addUser();
+      addUserCallback();
       arrLength = false;
     }
-    // eslint-disable-next-line
-  }, []);
+  }, [addUserCallback]);
 
   const next = () => {
     if (current.id < userList.length - 1) {
       setCurrent(userList[current.id + 1]);
     } else {
-      addUser();
+      addUserCallback();
     }
   };
 
